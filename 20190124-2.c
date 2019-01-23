696295256528e64c22d3febc30153b8c6967b2a5
채여민 <46066250+chaeyeomin@users.noreply.github.com>
2019-01-23 23:26:51 +0900

    Create 20190124-2.c

---
+++ 20190124-2.c
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include <iostream>
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	int b = 1;
+	int c ;
+	c = a * b;
+	scanf("%d", &a);
+	for (int i = 2; i < 10; i++) {
+		printf("%d \n", c);
+	}
+	return 0;
+}
