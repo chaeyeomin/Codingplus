023e1c0f79c617bc9dfcee87096ddddcd8b19533
채여민 <46066250+chaeyeomin@users.noreply.github.com>
2019-01-26 18:35:03 +0900

    Update 20190124-3.c

--- 20190124-3.c
+++ 20190124-3.c
@@ -1,20 +1,35 @@
-#include "pch.h"
-#include <iostream>
+#pragma warning(disable : 4996)
+
 #include<stdio.h>
 
+
+
 int main()
+
 {
-	int cnt1, cnt2;
+
+	int num;
+
+	int cnt = 0;
+
 	int sum = 0;
-	int a;
-	cnt1 = 1;
-	scanf("%d", &cnt2);
-		do {
-			printf("n 입력 : %d ", cnt2);
-			cnt1 += 1;
-			sum += cnt2;
-			++cnt2;
-		} while (cnt2 >= cnt1);
-		printf("합은 %d이다.", a);
+
+	printf("n 입력 : ");
+
+	scanf("%d", &num);
+
+	do {
+
+		cnt ++;
+
+		sum += cnt;  /* 만약 num에 5를 대입한다면 cnt는 1로 시작 -> 
+		                   sum은 1+1로 2가 됨 -> 아직 sum이 num보다 작으니까 다시 실행
+						   */
+
+	} while (cnt < num);
+
+	printf("1부터 %d까지의 합은 %d이다.", num, sum );
+
 	return 0;
+
 }
