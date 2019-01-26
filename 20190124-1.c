e717d4319103a43e2a2a39cc0a79f6d494e6db11
채여민 <46066250+chaeyeomin@users.noreply.github.com>
2019-01-26 17:55:41 +0900

    Update 20190124-1.c

--- 20190124-1.c
+++ 20190124-1.c
@@ -1,20 +1,39 @@
-#include "pch.h"
-#include <iostream>
+#pragma warning(disable : 4996)
+
 #include<stdio.h>
 
+
+
 int main()
+
 {
+
 	int a;
+
 	int b;
-	printf("첫 번째 숫자 입력 : %d \n", a);
+
+	printf("첫 번째 숫자 입력 : ");
+
 	scanf("%d", &a);
-	printf("두 번째 숫자 입력 : %d \n", b);
+
+	printf("두 번째 숫자 입력 : ");
+
 	scanf("%d", &b);
+
 	if (a < b) {
+
 		printf("두 번째 입력한 값이 더 크다.");
+
 	}
-	if (a = b) {
+	else if (a = b) {
+
 		printf("첫 번째 입력한 값과 두 번째 입력한 값이 같다.");
+
 	}
+	else {
+		printf("첫 번째 입력한 값이 더 크다.");
+
+	}
+
 	return 0;
 }
