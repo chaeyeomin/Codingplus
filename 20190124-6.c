25e8bc247cba4b42f6f73e19552cea1bfe652808
채여민 <46066250+chaeyeomin@users.noreply.github.com>
2019-01-26 19:28:15 +0900

    Create 20190124-6.c

---
+++ 20190124-6.c
@@ -0,0 +1,15 @@
+#pragma warning(disable : 4996)
+
+#include<stdio.h>
+#include <ctype.h>
+int main()
+
+{
+	char ch;
+	scanf("%c", &ch);
+	//ch = tolower(ch);
+	//ch = toupper(ch);
+	printf("%c", ch);
+	return 0;
+}
+	
