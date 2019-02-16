8bf31912e52f9bd7bb40c847931930cc4105657f
채여민 <46066250+chaeyeomin@users.noreply.github.com>
2019-02-16 17:08:18 +0900

    Create 20190216-3-1.c

---
+++ 20190216-3-1.c
@@ -0,0 +1,26 @@
+#pragma warning(disable : 4996)
+#include<stdio.h>
+
+int addplusoneWithPointer(int *a) {
+	*a += 1;
+	return *a;
+}
+
+int addplusone(int a) {
+	a += 1;
+	return a;
+}
+
+int arraySort(int * arr) {
+	arr[0] = 3;
+	return 0;
+}
+
+int main() {
+	int arr[5] = {0,1,2,3,4};
+	arraySort(arr);
+	int num;
+	//scanf("%d", &num);
+	//num = addplusone(num);
+	printf("num %d", arr[0]);
+}
