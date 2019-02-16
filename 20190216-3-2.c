a704627c1107f4fdd4fb9b340ec09f28fe80c06a
채여민 <46066250+chaeyeomin@users.noreply.github.com>
2019-02-16 17:09:17 +0900

    Create 20190216-3-2.c

---
+++ 20190216-3-2.c
@@ -0,0 +1,19 @@
+#pragma warning(disable : 4996)
+#include<stdio.h>
+
+int addplusoneWithPointer(int *a) {
+	*a += 1;
+	return *a;
+}
+
+int addplusone(int a) {
+	a += 1;
+	return a;
+}
+
+int main() {
+	int num;
+	scanf("%d", &num);
+	num = addplusone(num);
+	printf("num %d", num);
+}
